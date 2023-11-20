19b3f3a3e85424af1e8510970c6ad13e7a33ea36
Matt Kramer <mjkramer@users.noreply.github.com>
2023-11-19 19:47:32 -0800

    syntactic validity

--- pacmon.go
+++ pacmon.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	zmq "github.com/pebbe/zmq4/draft"
-
 )
 
-type SockOpt struct {
+type MsgType int
+
+const (
+	MsgTypeData MsgType = 'D'
+	MsgTypeRequest MsgType = '?'
+	MsgTypeReply MsgType = '!'
+)
+
+type WordType int
+
+const (
+	WordTypeData WordType = 'D'
+	WordTypeTrig WordType = 'T'
+	WordTypeSync WordType = 'S'
+	WordTypePing WordType = 'P'
+	WordTypeWrite WordType = 'W'
+	WordTypeRead WordType = 'R'
+	WordTypeError WordType = 'E'
+)
+
+type Word struct {
 
 }
+
+type Msg struct {
+	msgType MsgType
+	timestamp uint32
+	numWords uint16
+	words []Word
+}
+
+func (m *Msg) parse(raw []byte) *Msg {
+	m.msgType = MsgTypeData
+	m.timestamp = 1234
+	m.numWords = 0
+	m.words = []Word{}
+	return m
+}
+
 
 func main() {
 	fmt.Println("Hello, World!")
@@ -36,6 +72,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("yo %d %x\n", len(raw), raw)
+		// fmt.Printf("yo %d %x\n", len(raw), raw)
+		r := bytes.NewReader([]byte(raw))
 	}
 }
